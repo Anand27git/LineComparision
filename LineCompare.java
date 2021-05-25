a5ee4b21d645eb6503729abcf54b47398d45cbdd
Anand27git <anandkumar256k@gmail.com>
2021-05-25 19:25:19 +0530

    Compare the Two Lines are Equal or not

--- LineCompare.java
+++ LineCompare.java
@@ -20,12 +20,28 @@ public static void main (String args[]) {
 
 	double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 
+	 System.out.println("enter the values of next co-ordinates");
+		 System.out.println("Enter the value of (x3,y3)");
+		int x3 = sc.nextInt(); 
+		int y3 = sc.nextInt();
+		 System.out.println("Enter the Value of (x4,y4)");
+                int x4= sc.nextInt();
+                int y4= sc.nextInt();
+
+	 double distanceLine2=Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+
 	System.out.println("length of line1=" + distanceLine1);
+	 System.out.println("length of line2=" + distanceLine2);
 
-
+	if (distanceLine1==distanceLine2)
+            {
+ 		System.out.println("Two Lines are Equal");
+	     }
+		else {
+			System.out.println("Two Lines are not Equal");
+		      }
+		}
 	}
-
-    }
 
 
 
