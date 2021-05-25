81d7c26e0c9bf05c2905481d548816907b70a81f
Anand27git <anandkumar256k@gmail.com>
2021-05-25 19:03:46 +0530

    find the length of the line

--- LineCompare.java
+++ LineCompare.java
@@ -1,9 +1,31 @@
-public class LineCompare
-{
-public static void main(String[] args)
-{
-System.out.println("Welcome to Line Comparision Problems");
-}
+import java.util.Scanner;
 
-}
+public class LineCompare {
+
+	
+public static void main (String args[]) {
+
+
+	System.out.println("Welcome to Line Comparision Problems");
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the value co-ordinate");
+	System.out.println("---------------------------");
+	System.out.println("Enter the value of (x1,y1)");
+	 int x1= sc.nextInt();
+	 int y1= sc.nextInt();
+	System.out.println("Enter the Value of (x2,y2)");
+	  int x2= sc.nextInt();
+	  int y2= sc.nextInt();
+
+	double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+
+	System.out.println("length of line1=" + distanceLine1);
+
+
+	}
+
+    }
+
+
 
