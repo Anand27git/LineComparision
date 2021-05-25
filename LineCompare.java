b9f7666e393149b4f82cdd4891366d9e3274eaa8
Anand27git <anandkumar256k@gmail.com>
2021-05-25 19:48:58 +0530

    comparing two Lines

--- LineCompare.java
+++ LineCompare.java
@@ -21,7 +21,7 @@ public static void main (String args[]) {
 	double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 
 	 System.out.println("enter the values of next co-ordinates");
-		 System.out.println("Enter the value of (x3,y3)");
+	 System.out.println("Enter the value of (x3,y3)");
 		int x3 = sc.nextInt(); 
 		int y3 = sc.nextInt();
 		 System.out.println("Enter the Value of (x4,y4)");
@@ -33,13 +33,18 @@ public static void main (String args[]) {
 	System.out.println("length of line1=" + distanceLine1);
 	 System.out.println("length of line2=" + distanceLine2);
 
-	if (distanceLine1==distanceLine2)
+	if (distanceLine1<distanceLine2)
             {
- 		System.out.println("Two Lines are Equal");
+ 		System.out.println("Line 1 is less than Line 2");
 	     }
-		else {
-			System.out.println("Two Lines are not Equal");
+		else if (distanceLine1>distanceLine2)
+			{
+			System.out.println("Line1 is Greater than Line2");
 		      }
+			else
+			{
+			System.out.println("Both the  Lines are Equal");
+			}
 		}
 	}
 
