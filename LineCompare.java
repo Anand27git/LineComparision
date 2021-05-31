2c8b896e9651d2af925503be2a4efd54372aac24
Anand27git <anandkumar256k@gmail.com>
2021-05-31 18:49:21 +0530

    comparing the lines using oops concept

--- LineCompare.java
+++ LineCompare.java
@@ -1,52 +1,53 @@
 import java.util.Scanner;
 
-public class LineCompare {
+class LineComparision {
+	// declaring the variables
+	public int x1, x2, y1, y2;
 
-	
-public static void main (String args[]) {
+	public LineComparision(int x1, int x2, int y1, int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
 
-
-	System.out.println("Welcome to Line Comparision Problems");
-
-	Scanner sc = new Scanner(System.in);
-	System.out.println("enter the value co-ordinate");
-	System.out.println("---------------------------");
-	System.out.println("Enter the value of (x1,y1)");
-	 int x1= sc.nextInt();
-	 int y1= sc.nextInt();
-	System.out.println("Enter the Value of (x2,y2)");
-	  int x2= sc.nextInt();
-	  int y2= sc.nextInt();
-
-	double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-
-	 System.out.println("enter the values of next co-ordinates");
-	 System.out.println("Enter the value of (x3,y3)");
-		int x3 = sc.nextInt(); 
-		int y3 = sc.nextInt();
-		 System.out.println("Enter the Value of (x4,y4)");
-                int x4= sc.nextInt();
-                int y4= sc.nextInt();
-
-	 double distanceLine2=Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
-
-	System.out.println("length of line1=" + distanceLine1);
-	 System.out.println("length of line2=" + distanceLine2);
-
-	if (distanceLine1<distanceLine2)
-            {
- 		System.out.println("Line 1 is less than Line 2");
-	     }
-		else if (distanceLine1>distanceLine2)
-			{
-			System.out.println("Line1 is Greater than Line2");
-		      }
-			else
-			{
-			System.out.println("Both the  Lines are Equal");
-			}
-		}
 	}
 
+	public double calculateDistanceofLine() {
+		// calculating the line distance
+		double lineDist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return lineDist;
+	}
+}
 
+public class LineCompare {
 
+	public static void lineDiff(double length1, double length2) {
+		// using if condition we comparingthe lenghts of the lines are equal or not
+		if (length1 < length2) {
+			System.out.println("Line 1 is less than Line 2");
+		} else if (length1 > length2) {
+			System.out.println("Line1 is Greater than Line2");
+		} else {
+			System.out.println("Both the  Lines are Equal");
+		}
+
+	}
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to Line Comparision Problems");
+
+		LineComparision line1 = new LineComparision(3, 6, 4, 8);
+
+		double length1 = line1.calculateDistanceofLine();
+		System.out.println("Length of Line-1=" + length1);
+
+		LineComparision line2 = new LineComparision(11, 8, 7, 9);
+
+		double length2 = line2.calculateDistanceofLine();
+		System.out.println("Length of Line-2=" + length2);
+
+		lineDiff(length1, length2);
+
+	}
+}
